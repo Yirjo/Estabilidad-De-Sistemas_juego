eaf2008bbdfcdbbfb23a71c98f122093e4ddedc0
Yirjo <ospinogomezyairjose@gmail.com>
2025-11-19 21:40:16 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -32,7 +32,7 @@ let zoneHeight = 150;
 // Oscilación del setpoint
 let oscillationTime = 0;
 let oscillationSpeed = 0.010;
-let oscillationAmplitude = 200;
+let oscillationAmplitude = 120;
 
 // Estado
 let isGameOver = false;
